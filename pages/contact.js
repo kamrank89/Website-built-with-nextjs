b7ae61be37b14508a41e46aa1314153f94a00712
kamran <khosravi42@gmail.com>
2022-10-09 10:40:25 -0500

    scrollbar modification to global.css

--- pages/contact.js
+++ pages/contact.js
@@ -3,9 +3,58 @@ import Header from "../components/header/header";
 
 export default function contact() {
   return (
-    <div className="min-h-screen flex flex-col bg-slate-200">
+    <div className="min-h-screen flex scrollbar scrollbar-thumb-blue-400 scrollbar-rounded flex-col bg-slate-200">
       <Header />
-      <h1>this is contact page </h1>
+      <div>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+        <h1>this is contact page </h1>
+      </div>
 
       <div className="mt-auto">
         <Footer />
